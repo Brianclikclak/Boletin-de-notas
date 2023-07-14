9cb1a67d3d2b189ea0ab78a781990f176ccc9c7f
Brianclikclak <brianclikclak@hotmail.com>
2023-07-14 18:51:30 +0200

    src

---
+++ src/ClassObject.js/StudentObject.js
@@ -0,0 +1,36 @@
+export default class StudenTemplate{
+    #name;
+    #subject;
+    #qualification;
+
+    constructor(name, subject, qualification){
+            this.name = name
+            this.subject = subject;
+            this.qualification = qualification;
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    set name(name) {
+        this.#name = name;
+    }
+
+    get subject(){
+        return  this.#subject;
+    }
+
+    set subject(subject) {
+        this.#subject = subject;
+    }
+
+    get qualification() {
+        return this.#qualification;
+    }
+
+    set qualification(qualification){
+        this.#qualification = qualification;
+    }
+
+}
