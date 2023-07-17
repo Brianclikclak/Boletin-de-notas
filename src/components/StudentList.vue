7841605c8f10012e6d0c81d35e09a069a45a0d9a
Brianclikclak <brianclikclak@hotmail.com>
2023-07-17 17:48:14 +0200

    component finish

---
+++ src/components/StudentList.vue
@@ -0,0 +1,44 @@
+<script setup>
+	import { qualificationInToGrades } from "../qualification/qualification";
+	import { ref } from "vue";
+	
+	const props = defineProps({
+		students: Array,
+		datos: {},
+	});
+	
+</script>
+<template>
+	<div v-show="students.length > 0 " class="container">
+		<table class="container__table">
+			<thead>
+				<tr class="container__titles">
+					<th class="container__keys">Name</th>
+					<th class="container__keys">subject</th>
+					<th class="container__keys">Calification</th>
+				</tr>
+			</thead>
+			<tbody class="container__students">
+				<tr v-for="{ name, subject, qualification } in students" :key="name">
+					<td class="container__value">{{ name }}</td>
+					<td
+						class="container__value"
+						
+					>
+						{{ subject }}
+					</td>
+					<td 
+						class="container__value" 
+						
+					>
+						{{ qualificationInToGrades(qualification) }}
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<style lang="scss">
+
+</style>
