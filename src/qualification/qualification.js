3e6bbea0b220074b92f1036a7c72697ce546dc66
Brianclikclak <brianclikclak@hotmail.com>
2023-07-17 17:45:20 +0200

    function to change grades

---
+++ src/qualification/qualification.js
@@ -0,0 +1,10 @@
+export const qualificationInToGrades = num => {
+    if (num < 0) return `"${num}", no es una nota válida`;
+	if (num > 10) return `"${num}", no es una nota válida`;
+	if (num <= 3) return "Muy deficiente";
+	if (num < 5) return "Insuficiente";
+	if (num <= 6) return "Suficiente";
+	if (num <= 8) return "Notable";
+	if (num <= 10) return "Sobresaliente";
+
+};
