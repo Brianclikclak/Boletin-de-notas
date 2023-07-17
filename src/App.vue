d2342cbed21ac8cbd658a8eb4a663aad63dfd43a
Brianclikclak <brianclikclak@hotmail.com>
2023-07-17 17:50:45 +0200

    ohter hotfix

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+	import { getCurrentInstance, ref, readonly } from "vue";
+	import StudentForm from "./components/StudentForm.vue";
+	import StudentList from "./components/StudentList.vue";
+
+	let studentList = readonly([]);
+	let componentListKey = ref(0);
+	const instance = getCurrentInstance();
+
+	function retrieveData(data) {
+		studentList = data;
+		forceRerenderList();
+		instance.proxy.$forceUpdate();
+	}
+
+	function forceRerenderList() {
+		componentListKey += 1;
+	}
 </script>
 
 <template>
-  <header>
-    
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	<StudentForm @send-data="retrieveData" />
+	<StudentList
+		:key="componentListKey"
+		:students="studentList"
+		
+	/>
 </template>
 
 <style scoped>
-
+	body {
+		width: 100vh;
+		height: 100vh;
+	}
 </style>
